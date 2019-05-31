2572a3bd88b4c256cac09effa41bad4cbca5df41
Sunghyun Kim <sunghyun_k@icloud.com>
2019-05-31 09:53:12 +0900

    Suit 열거형 및 카드 구조체 작성

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -1,19 +1,46 @@
 import Foundation
 
-/// 카드 숫자는 종류가 13가지로 제한되어 있기 때문에 열거형을 선택함.
-enum Rank: Int {
-    case ace = 1
-    case two, three, four, five, six, seven, eight, nine, ten
-    case jack, queen, king
-    
-    var description: String {
-        switch self {
-        case .ace: return "A"
-        case .jack: return "J"
-        case .queen: return "Q"
-        case .king: return "K"
-        default: return String(self.rawValue)
+/// 인스턴스를 전달 또는 할당할 시에 값이 복사되도록 구조체를 선택함.
+struct Card: CustomStringConvertible {
+    /// 카드 숫자는 종류가 13가지로 제한되어 있기 때문에 열거형을 선택함.
+    enum Rank: Int {
+        case ace = 1
+        case two, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king
+        
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .jack: return "J"
+            case .queen: return "Q"
+            case .king: return "K"
+            default: return String(self.rawValue)
+            }
         }
     }
+    
+    /// 무늬의 종류가 4가지로 제한되어 있기 때문에 열거형을 선택함.
+    enum Suit {
+        case spades, hearts, diamonds, clubs
+        
+        var symbol: String {
+            switch self {
+            case .spades:
+                return "♠"
+            case .hearts:
+                return "♥"
+            case .diamonds:
+                return "♦"
+            case .clubs:
+                return "♣"
+            }
+        }
+    }
+    
+    var rank: Rank
+    var suit: Suit
+    
+    var description: String {
+        return "\(suit.symbol) \(rank.description)"
+    }
 }
-
